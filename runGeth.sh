98df715622a1671dfe1e759af226add3526b9a6d
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-09-17 19:09:58 -0400

    update

--- runGeth.sh
+++ runGeth.sh
@@ -2,9 +2,10 @@
 port=8545
 #localhost, 8545
 ufw deny $port;
+ufw allow $port;
 while true
 do
         #sudo nohup js app.js > /dev/null
-	sudo nohup geth --syncmode 'fast' --rpc --rpcapi 'personal,db,eth,net,web3,txpool' --datadir "/root/distribution-form/ethdata"
+	sudo nohup geth --syncmode 'fast' --rpc --rpcapi 'personal,db,eth,net,web3,txpool' --rpcaddr '0.0.0.0' --datadir "/root/distribution-form/ethdata"
 done
 
