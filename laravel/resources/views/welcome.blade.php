e188af716970f606808a25499d515d8f91077678
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-09-21 15:42:13 -0400

    fix

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -111,7 +111,6 @@
                 text-decoration: underline;
                 color: #666;
             }
-
             .btn-register {
                 background-color: #1CB94E;
                 outline: none;
@@ -184,7 +183,7 @@
                                             <form id="login-form" action="{{ route('login') }}" method="post" role="form" style="display: block;">
                                                 {{ csrf_field() }}
                                                 <div class="form-group">
-                                                    <input type="text" name="name" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+                                                    <input type="email" name="email" id="username" tabindex="1" class="form-control" placeholder="  Email" value="">
                                                 </div>
                                                 <div class="form-group">
                                                     <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
@@ -213,7 +212,7 @@
                                             <form id="register-form" action="{{ url('/signup') }}" method="post" role="form" style="display: none;">
 
                                                 {{ csrf_field() }}
-                                                 <input type="hidden" name="avatar" value="" id="avatar" >     
+
                                                 <div class="form-group">
                                                     <input type="text" name="name" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
                                                 </div>
@@ -257,34 +256,18 @@
             
         </div>
 
+
         <!-- Javascript -->
         <script src="{{ URL::asset('assets/js/jquery-1.11.1.min.js')}}"></script>
         <script src="{{ URL::asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
         <script src="{{ URL::asset('assets/js/jquery.backstretch.min.js')}}"></script>
         <script src="{{ URL::asset('assets/js/scripts.js')}}"></script>
-                <script src="{{ URL::asset('assets/js/pnglib.js')}}"></script>
-
-        <script src="{{ URL::asset('assets/js/identicon.js')}}"></script>
-
-       
+        
+        <!--[if lt IE 10]>
+            <script src="{{ URL::asset('assets/js/placeholder.js')}}"></script>
+        <![endif]-->
         <script type="text/javascript">
-             
-              function randomString(length, chars) {
-                var result = '';
-                for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
-                return result;
-              }
-           
-              $(function() {
-              
-                var hash = randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');// 15+ hex chars
-
-                var data = new Identicon(hash, 48).toString();
-                var avatar='data:image/png;base64,'+data;
-                //document.write('<img  src="' + avatar + '">');
-                  
-               $("#avatar").val(avatar);
-            
+            $(function() {
                 $('#login-form-link').click(function(e) {
                     $("#login-form").delay(100).fadeIn(100);
                     $("#register-form").fadeOut(100);
@@ -292,7 +275,6 @@
                     $(this).addClass('active');
                     e.preventDefault();
                 });
-                
                 $('#register-form-link').click(function(e) {
                     $("#register-form").delay(100).fadeIn(100);
                     $("#login-form").fadeOut(100);
@@ -300,7 +282,6 @@
                     $(this).addClass('active');
                     e.preventDefault();
                 });
-
             });
         </script>
 
