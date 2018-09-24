94c8226e36c7e6d38e6bb3622f93e308f647e657
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-09-24 19:03:23 -0400

    i

--- backupDatabases.sh
+++ backupDatabases.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 # Used to backup databases so that we can keep a common username/passwords for example users
 # Can be used to create sample users with different arrows and karmas easier
+if [ $# -eq 0 ] ; then
+    echo 'Please enter a name-ending for the database'
+    exit 1
+fi
+
+name=$1;
 chmod 400 sshKey
-ssh -i "./sshKey" root@distribution.projectoblio.com "mysqldump -uroot -panyPassword msf > ~/databaseBackup1.sql";
-scp -i "./sshKey" root@distribution.projectoblio.com:~/databaseBackup1.sql ./backupDatabases/
+echo "Saving database to databaseBackup$name.sql";
+ssh -i "./sshKey" root@distribution.projectoblio.com "mysqldump -uroot -panyPassword msf > ~/databaseBackup$name.sql";
+scp -i "./sshKey" root@distribution.projectoblio.com:~/databaseBackup$name.sql ./backupDatabases/
