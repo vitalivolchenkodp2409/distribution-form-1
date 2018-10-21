37dd8259bda8e3f9cb280dee26f8930a174f5593
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-10-21 00:24:49 -0400

    try

--- localUpdate.sh
+++ localUpdate.sh
@@ -40,7 +40,7 @@ chmod -R 775 /var/www/laravel/bootstrap/cache;
 
     		echo 'Restoring mysql backup'
    		mysql -u$dbuser -p$dbpass -h$dbhost -e "drop database $dbname;" 
-		#mysql -u$dbuser -p$dbpass -h$dbhost -e "create database $dbname;" 
+		mysql -u$dbuser -p$dbpass -h$dbhost -e "create database $dbname;" 
 	    	#mysql -u$dbuser -p$dbpass -h$dbhost $dbname < /root/distribution-form/backupDatabases/databaseBackup1.sql;
 	fi
 fi
