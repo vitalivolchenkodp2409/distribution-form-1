612e0c2cbc98e81feff67ee8e9b9172e380354a5
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-10-21 00:35:08 -0400

    try

--- laravel/app/User.php
+++ laravel/app/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','avatar','ip','arrows','karma','last_dub_time',
+        'name', 'email','username', 'password','avatar','ip','arrows','karma','last_dub_time',
     ];
 
     /**
