883015a7ad8380eb4b7e581130635cf7be8672d0
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-10-21 00:11:47 -0400

    try

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,22 +15,34 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name')->unique();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('type')->nullable();
-            $table->string('avatar')->nullable();
-            $table->string('arrows')->default('0')->nullable();
-            $table->string('karma')->default('0')->nullable();
-            $table->string('last_dub_time')->default('0')->nullable();
-            $table->string('point')->default('0')->nullable();
-	        //$table->string('karma')->default('0')->nullable();
-	        //$table->string('last_dub_time')->default('0')->nullable();
-            $table->integer('form_level')->default(0)->nullable();
-            $table->string('ip')->nullable();
-            $table->rememberToken();
-            $table->timestamps();
+
+		// standard across sites:
+		$table->increments('id');
+		$table->string('name')->unique();
+		$table->string('email')->unique();
+		$table->string('password');
+		$table->string('arrows')->default('0')->nullable();
+		$table->string('karma')->default('0')->nullable();
+		$table->string('communityPoints')->default('0')->nullable();
+		$table->string('last_dub_time')->default('0')->nullable();
+		$table->string('avatar')->nullable();
+		$table->rememberToken();
+		$table->timestamps();
+
+		// distribution only:
+		$table->string('type')->nullable();
+		$table->string('point')->default('0')->nullable();
+		$table->integer('form_level')->default(0)->nullable();
+		$table->string('ip')->nullable();
+
+		// music only: 
+		$table->boolean('is_admin')->default(false);
+
+		// poster site / laravel-irt /lolhow only: 
+		$table->string('api_token', 60)->unique();
+		$table->integer('thread_karma')->default(0);
+		$table->integer('post_karma')->default(0);
+          
         });
     }
 
