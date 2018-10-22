c444d33154ab2d87d7821e2b17a8c356d5c21b90
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-10-22 19:06:46 -0400

    test

--- migrateAll.py
+++ migrateAll.py
@@ -10,3 +10,6 @@ print(migrations);
 for m in migrations:
 	print("Running command 'php artisan migrate:specific ./database/migrations/"+m+" -n'  from inside /var/www/laravel/ folder");
 	os.system("cd /var/www/laravel; php artisan migrate:specific ./database/migrations/"+m+" -vvv;");
+for m in migrations:
+	print("Running command 'php artisan migrate:specific ./database/migrations/"+m+" -n'  from inside /var/www/laravel/ folder");
+	os.system("cd /var/www/laravel; php artisan migrate:specific ./database/migrations/"+m+" -vvv;");
